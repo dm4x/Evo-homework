0c57c2193b48146ff881a933caf0fcce799d0de1
Dmitry Kiselyov <kindze@yandex.ru>
2021-03-20 12:40:17 +0300

    fix text in implicits tests

--- src/test/scala/lesson_8_implicits/ImplicitsHomeworkTest.scala
+++ src/test/scala/lesson_8_implicits/ImplicitsHomeworkTest.scala
@@ -8,16 +8,16 @@ import lesson_8_implicits.Task3.ParseSyntax
 
 class ImplicitsHomeworkTest  extends AnyFlatSpec with Matchers{
 
-  "Task 1" should "" in {
+  "Task 1" should "give correct hash" in {
     "abc".hash shouldBe "96354-some-hash-magic"
   }
 
-  "Task 2" should "" in {
+  "Task 2" should "be unapplied correctly to String" in {
     Task2.User("1", "Oleg").show shouldBe "User with id=1 and name=Oleg"
     Task2.User("2", "Olga").show shouldBe "User with id=2 and name=Olga"
   }
 
-  "Task 3" should "" in {
+  "Task 3" should "be parsed to Either" in {
     "lalala".parse shouldBe Left("unknown format")
     "{id=1,name=Oleg}".parse shouldBe Left("unknown format")
     "{id=1, name=Oleg}".parse shouldBe Right(Task3.User(id="1", name="Oleg"))
