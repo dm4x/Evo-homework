a8ed15cbdcdf02bd4b7cf70f01b01d55fb8550b6
Dmitry Kiselyov <kindze@yandex.ru>
2021-03-24 12:53:35 +0300

    fix linter problems

--- src/main/scala/lesson_14_cats_core/Exercises.scala
+++ src/main/scala/lesson_14_cats_core/Exercises.scala
@@ -69,20 +69,21 @@ object MonadHierarchy {
   // monad:        F[F[A]]     => F[A]
 }
 
-object Excercises extends App{
+object Excercises extends App {
+
   trait Applicative[F[_]] extends Functor[F] {
-    def map[A,B](fa: F[A])(f: A => B): F[B]
+    def map[A, B](fa: F[A])(f: A => B): F[B]
 
     def unit[A](a: => A): F[A]
 
     // implement methods using each other
-    def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = apply( apply(unit(f.curried)) (fa) ) (fb)
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = apply(apply(unit(f.curried))(fa))(fb)
 
-    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = map2(fa, fab)( (a, f) => f(a))
+    def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fa, fab)((a, f) => f(a))
 
     def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(identity)
 
-    def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = sequence(as.map(f))
+    def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] = sequence(as.map(f))
   }
 
   trait Monad[M[_]] extends Functor[M] {
@@ -97,4 +98,5 @@ object Excercises extends App{
 
     def map2[A, B, C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] = flatMap(ma)(a => map(mb)(b => f(a, b)))
   }
+
 }
